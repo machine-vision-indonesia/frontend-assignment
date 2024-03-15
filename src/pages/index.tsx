d68410d8e19b13b61a15fc5c46b22b43c1438807
Hasan Nasrullah <hnasrul22@gmail.com>
2024-03-15 15:07:38 +0700

    feat: integration bei on table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,37 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Table from "@/components/Table/Index";
+import { GetListBEI } from "@/service/bei/index.service";
 
 export default function Home() {
+
+    const columns = [
+        {
+            field: "id",
+            headerName: "ID",
+            flex: 1
+        },
+        {
+            field: "kode_saham",
+            headerName: "Kode",
+            flex: 1
+        },
+        {
+            field: "nama",
+            headerName: "Nama",
+            flex: 1
+        },
+        {
+            field: "tanggal_listing",
+            headerName: "Tanggal Listing",
+            flex: 1
+        },
+        {
+            field: "sektor_nama",
+            headerName: "Sektor",
+            flex: 1
+        },
+    ]
     return (
         <Box
             sx={{
@@ -16,9 +45,10 @@ export default function Home() {
                 alignItems: "center",
             }}
         >
-            <Typography variant="h4" component="h1" sx={{mb: 2}}>
-                Hello world
-            </Typography>
+            <Table
+                columns={columns}
+                dataFetchService={GetListBEI}
+            />
         </Box>
     );
 }
